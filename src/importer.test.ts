3655a30976d19583fdb6ebb2c97bdcccfd6da7e4
mcasarrubios <miguel.garcia.casarrubios@gmail.com>
2021-06-14 18:20:36 +0200

    chore(setup): refactor support files

--- src/importer.test.ts
+++ src/importer.test.ts
@@ -1,34 +1,26 @@
 import { Importer } from './importer';
 
-describe('demo', () => {
+describe('importer', () => {
   it('should import successfully the import-success file', async () => {
     const importer = new Importer();
     const result = await importer.import('import-success.csv');
     expect(result.ok.length).toBe(2);
     expect(result.ko.length).toBe(0);
-    expect(result.ok[0]).toBe({
+    expect(result.ok[0]).toMatchObject({
       code: 'F001',
-      issuedDate: '2021/04/17',
-      owner: {
-        name: 'John Doe S.L.',
-      },
-      contact: {
-        name: 'Jane Roe',
-      },
+      issuedDate: '2021-04-17',
+      ownerName: 'John Doe S.L.',
+      contactName: 'Jane Roe',
       subtotal: 100.0,
       taxes: 21.0,
       total: 121.0,
       status: 'issued',
     });
-    expect(result.ok[1]).toBe({
+    expect(result.ok[1]).toMatchObject({
       code: 'F002',
-      issuedDate: '2021/04/18',
-      owner: {
-        name: 'John Doe S.L.',
-      },
-      contact: {
-        name: 'Jane Roe',
-      },
+      issuedDate: '2021-04-18',
+      ownerName: 'John Doe S.L.',
+      contactName: 'Jane Roe',
       subtotal: 200.0,
       taxes: 42.0,
       total: 242.0,
@@ -41,25 +33,43 @@ describe('demo', () => {
     const result = await importer.import('import-with-errors.csv');
     expect(result.ok.length).toBe(1);
     expect(result.ko.length).toBe(4);
-    expect(result.ko[0]).toBe({
-      line: 2,
-      field: 'status',
-      message: 'invalid',
-    });
-    expect(result.ko[1]).toBe({
+    expect(result.ko[0]).toEqual(
+      expect.objectContaining({
+        line: 2,
+        errors: [
+          {
+            property: 'status',
+            message: 'invalid',
+          },
+        ],
+      }),
+    );
+    expect(result.ko[1]).toMatchObject({
       line: 3,
-      field: 'owner.name',
-      message: 'required',
+      errors: [
+        {
+          property: 'ownerName',
+          message: 'required',
+        },
+      ],
     });
-    expect(result.ko[2]).toBe({
+    expect(result.ko[2]).toMatchObject({
       line: 4,
-      field: 'code',
-      message: 'required',
+      errors: [
+        {
+          property: 'code',
+          message: 'required',
+        },
+      ],
     });
-    expect(result.ko[3]).toBe({
+    expect(result.ko[3]).toMatchObject({
       line: 5,
-      field: 'total',
-      message: 'invalid',
+      errors: [
+        {
+          property: 'total',
+          message: 'invalid',
+        },
+      ],
     });
   });
 });
