b456c864c0bdb9bdba1f1bc64ce5bc726659dfb7
kno <linsms@gmail.com>
2022-01-20 19:53:30 +0100

    Importer working

--- src/importer.ts
+++ src/importer.ts
@@ -1,12 +1,50 @@
+import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
+import { parse } from 'csv-parse';
+import fs from 'fs';
+import { Record, Error, importerResult } from './types';
+import { schema, columns, formatError, caster } from './validation';
+
 export class Importer {
-  async import(filePath: string): Promise<any> {
-    console.log(
-      'remove this line when you use the filePath variable',
-      filePath,
-    );
-    return {
-      ok: [],
-      ko: [],
-    };
+  ok: Record[] = [];
+  ko: Error[] = [];
+  ajv = new Ajv();
+
+  async import(filePath: string): Promise<importerResult> {
+    addFormats(this.ajv);
+    const promise = new Promise<importerResult>((resolve) => {
+      const parser = parse({
+        fromLine: 2,
+        trim: true,
+        cast: caster,
+        delimiter: ';',
+        columns: columns,
+      });
+      fs.createReadStream(`files/${filePath}`).pipe(parser);
+      let line = 0;
+      const validate = this.ajv.compile(schema);
+      parser.on('readable', () => {
+        let record;
+        while ((record = parser.read()) !== null) {
+          line++;
+          const valid = validate(record);
+          if (valid) {
+            this.ok.push(record);
+          } else {
+            this.ko.push({
+              line: line,
+              errors: validate.errors?.map(formatError) || [],
+            });
+          }
+        }
+      });
+      parser.on('end', () => {
+        resolve({
+          ok: this.ok,
+          ko: this.ko,
+        });
+      });
+    });
+    return promise;
   }
 }
